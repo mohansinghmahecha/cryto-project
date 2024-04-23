71b69a15a2fc0be8467c2268f5bf0d1a0e936186
Mohan Singh Mahecha <mohansinghmahecha2000@gmail.com>
2024-04-23 20:36:35 +0530

    store changes

--- src/Redux/State/store/index.js
+++ src/Redux/State/store/index.js
@@ -3,7 +3,7 @@ import { createStore, combineReducers } from "redux";
 import { MyproductList } from "../reducer";
 import { MyCurrencyNames } from "../reducer";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ;
+//const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ;
 
 export const store = createStore(
   combineReducers({
