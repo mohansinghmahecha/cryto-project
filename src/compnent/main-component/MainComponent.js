4b91561849926be49a8671fb883e50286cf08f78
Mohan Singh Mahecha <mohansinghmahecha2000@gmail.com>
2024-04-23 12:04:25 +0530

    last commit

--- src/compnent/main-component/MainComponent.js
+++ src/compnent/main-component/MainComponent.js
@@ -3,16 +3,14 @@ import ListOfTradingItems from "./ListOfTradingItems";
 
 export default function MainComponent() {
     return (
-        <div style={{ width: '100%', display: 'flex', marginTop: '5rem' }}>
-            {/* Chat component */}
-            <div style={{ width: '75%' }}>
-                <Header />
-            </div>
-
-            {/* List chart component */}
-            <div style={{ width: '25%' }}>
-                <ListOfTradingItems />
-            </div>
+        <div className="wrapper" style={{ width: '100%', display: 'flex', marginTop: '5rem', flexWrap: 'wrap'}}>
+        <div style={{ flex: '1 1 70%' }}> {/* s Use flex-grow, flex-shrink, and flex-basis */}
+            <Header />
         </div>
+        <div style={{ flex: '1 1 25%' }}>
+            <ListOfTradingItems />
+        </div>
+    </div>
+    
     );
 }
